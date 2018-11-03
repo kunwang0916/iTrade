464e6903ae50b60ab05acbd5faa809e660bf83da
kun wang <>
2018-11-03 01:05:26 -0700

    Updates

---
+++ precache-manifest.c0926980244376e66bd01b4786cdb320.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2af28f6524b3874b81b",
+    "url": "/iTrade/static/js/runtime~main.b2af28f6.js"
+  },
+  {
+    "revision": "adec45359e0cb36bc3e2",
+    "url": "/iTrade/static/js/main.adec4535.chunk.js"
+  },
+  {
+    "revision": "19940f1664c9995d4eb6",
+    "url": "/iTrade/static/js/1.19940f16.chunk.js"
+  },
+  {
+    "revision": "adec45359e0cb36bc3e2",
+    "url": "/iTrade/static/css/main.09f53844.chunk.css"
+  },
+  {
+    "revision": "d5c7dfdea1280319def55a25ed6f1287",
+    "url": "/iTrade/index.html"
+  }
+];
