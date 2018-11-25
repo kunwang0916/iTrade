eac6e6847298be110346890b843d1353018244f0
kun wang <>
2018-11-24 20:26:50 -0800

    change the iTrade auth redirect page.

--- frontend/src/pages/SignInPage.js
+++ frontend/src/pages/SignInPage.js
@@ -12,7 +12,7 @@ class SignInPage extends Component {
       // Popup signin flow rather than redirect flow.
       signInFlow: 'popup',
       // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-      signInSuccessUrl: '/',
+      signInSuccessUrl: '/iTrade/',
       // We will display Google and Facebook as auth providers.
       signInOptions: [
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
